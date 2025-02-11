80e667d0d69d6ece3213632e2abc8c9af7495ba2
xeryux-rd <147223983+xeryux-rd@users.noreply.github.com>
2025-02-11 09:30:56 +0700

    Export Vue

---
+++ src/components/KepalaView.vue
@@ -0,0 +1,15 @@
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-title>
+                {{title}}
+                <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+            </ion-title>
+        </ion-toolbar>
+    </ion-header>
+</template>
+<script setup>
+defineProps({
+    title:String,
+})
+</script>
